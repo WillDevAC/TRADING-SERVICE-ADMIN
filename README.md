653e9535d526199456cc3f121cda8fbe923ac7e7
Jefté Natã <jefte.medeiros8@gmail.com>
2022-04-28 15:42:07 -0500

    first-commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Next.JS template with Tailwind styled components
--- src/components/templates/consultant/ferraments/global.ts
+++
@@ -1,5 +0,0 @@
-import tw from 'tailwind-styled-components'
-
-export const Ferrament = tw.li `
-    col-span-1 flex shadow-sm rounded-md items-center justify-center cursor-pointer bg-white p-4
-`
