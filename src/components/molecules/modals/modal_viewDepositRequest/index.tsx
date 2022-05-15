6f3d7d61cef32e8a027d9405a3d7f79a588da37e
luissantosjs <dasilvasantosluisfelipe@gmail.com>
2022-05-15 12:24:25 -0500

    update

--- src/components/molecules/modals/modal_viewDepositRequest/index.tsx
+++ src/components/molecules/modals/modal_viewDepositRequest/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { DepositData } from "../../../../pages/painel/admin/deposit_requests";
 
 import {
   Modal,
@@ -9,15 +10,38 @@ import {
   ButtonDecline,
   ButtonAccept,
   ButtonExclude,
-  ButtonDocument
+  ButtonDocument,
+  Label,
+  Input,
 } from "../global";
 
 interface IProps {
   modal: boolean;
   setModal: Function;
+  request?: DepositData;
 }
 
-const modal_view_deposit: React.FC<IProps> = ({ modal, setModal }) => {
+const modal_view_deposit: React.FC<IProps> = ({ modal, setModal, request }) => {
+  const [confirmedValue, setConfirmedValue] = useState(0);
+  useEffect(() => {
+    if (!!request) {
+      setConfirmedValue(
+        parseFloat(request?.confirmedAmount) > 0
+          ? parseFloat(request?.confirmedAmount)
+          : parseFloat(request?.amount)
+      );
+    }
+  }, [request]);
+
+  const getValueBRL = useCallback(() => {
+    return (parseFloat(request?.priceBtc) * confirmedValue).toLocaleString(
+      "pt-BR",
+      {
+        style: "currency",
+        currency: "BRL",
+      }
+    );
+  }, [confirmedValue, request]);
   return (
     <>
       {modal == true && (
@@ -28,30 +52,60 @@ const modal_view_deposit: React.FC<IProps> = ({ modal, setModal }) => {
               <h2 className="font-bold text-lg">Informações do déposito</h2>
               <div className="pt-5">
                 <p>Solicitante: </p>
-                <b>Wydenmairion silva da rocha</b>
-              </div>
-
-              <div className="pt-5">
-                <p>Saldo atual: </p>
-                <b>R$ 0.00</b>
+                <b>{request?.user?.name}</b>
               </div>
 
               <div className="pt-5">
                 <p>Valor solicitado: </p>
-                <b>R$ +0.00</b>
+                <b>{request?.amount + " BTC"}</b>
+              </div>
+
+              <div className="pt-5">
+                <p>Preço do BTC quando solicitado: </p>
+                <b>
+                  {parseFloat(request?.priceBtc).toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
+                </b>
+              </div>
+
+              <div className="pt-5">
+                <p>Valor em Reais: </p>
+                <b>{getValueBRL()}</b>
+              </div>
+
+              <div className="pt-5">
+                <Label>Valor confirmado em BTC</Label>
+                <Input
+                  type="number"
+                  onChange={(e) => setConfirmedValue(e.target.value)}
+                  disabled={
+                    request.status.description === "pending" ? false : true
+                  }
+                  value={confirmedValue}
+                />
               </div>
 
               <div className="pt-5">
                 <p>Comprovante: </p>
                 <div className="pt-5">
-                <ButtonDocument>Ver comprovante</ButtonDocument>
+                  <ButtonDocument onClick={()=>{
+                       window.open(request?.receiptUrl, '_blank')
+                  }}>Ver comprovante</ButtonDocument>
                 </div>
               </div>
             </ModalBody>
             <ModalFooter>
-              <ButtonDecline onClick={() => setModal(false)}>Fechar</ButtonDecline>
-              <ButtonAccept>Aceitar</ButtonAccept>
-              <ButtonExclude>Recusar</ButtonExclude>
+              <ButtonDecline onClick={() => setModal(false)}>
+                Fechar
+              </ButtonDecline>
+              {request.status.description === "pending" && (
+                <ButtonAccept>Aceitar</ButtonAccept>
+              )}
+              {request.status.description === "pending" && (
+                <ButtonExclude>Recusar</ButtonExclude>
+              )}
             </ModalFooter>
           </ModalWrapper>
         </Modal>
--- src/pages/painel/admin/deposit_requests/index.tsx
+++ src/pages/painel/admin/deposit_requests/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import LayoutFragment from "../../../../components/layout/admin";
 
@@ -19,16 +19,61 @@ import {
   TableBody,
   Button,
 } from "../../../../components/molecules/table/global";
-
+import { toast } from "react-nextjs-toast";
 import ModalViewRequest from "../../../../components/molecules/modals/modal_viewDepositRequest";
+import { api } from "../../../../services/api";
+
+export interface DepositData {
+  id: string;
+  user: {
+    name: string;
+  };
+  status: {
+    description: string;
+  };
+  priceBtc: string;
+  confirmedAmount: string;
+  receiptUrl: string;
+  amount: string;
+}
 
 const Deposit_requests: React.FC = () => {
   const [modalView, setModalView] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(true);
   const take = 10;
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<DepositData[]>([]);
   const [skip, setSkip] = useState<number>(0);
+  const [count, setCount] = useState(0);
   const [search, setSearch] = useState<string>("");
+  const [id, setId] = useState(null)
+
+  const onLoad = async () => {
+    const response = await api.get(
+      `/transaction/request-deposit?take=${take}&skip=${
+        skip * take
+      }&search=${search}`,
+      {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("@token"),
+        },
+      }
+    );
+    if (!response.data.message) {
+      setData(response.data.data);
+      console.log(response.data);
+      setCount(Math.ceil(response.data.count / take));
+    } else {
+      toast.notify(response.data?.message, {
+        title: "error",
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window) {
+      onLoad();
+    }
+  }, [typeof window, skip, search, modalView]);
 
   return (
     <LayoutFragment
@@ -47,21 +92,37 @@ const Deposit_requests: React.FC = () => {
                     <Row>
                       <ColumnTh scope="col">Nome do investidor</ColumnTh>
                       <ColumnTh scope="col">Valor solicitado</ColumnTh>
-                      <ColumnTh scope="col">Saldo atual</ColumnTh>
+                      <ColumnTh scope="col">
+                        Preço do BTC quando solicitado
+                      </ColumnTh>
+                      <ColumnTh scope="col">Valor em Reais</ColumnTh>
                       <ColumnTh scope="col">Ações</ColumnTh>
                     </Row>
                   </TableHead>
                   <TableBody>
-                    <Row>
-                      <ColumnTd>JOSE SANTOS DA ROCHA</ColumnTd>
-                      <ColumnTd>R$ +500.00</ColumnTd>
-                      <ColumnTd>R$ 0.00</ColumnTd>
-                      <ColumnTd>
-                        <Button onClick={() => setModalView(true)}>
-                          Detalhes
-                        </Button>
-                      </ColumnTd>
-                    </Row>
+                    {data.map((res, i) => {
+                      return (
+                        <Row key={i}>
+                          <ColumnTd>{res.user.name}</ColumnTd>
+                          <ColumnTd>{res.amount + " BTC"}</ColumnTd>
+                          <ColumnTd>
+                            {parseFloat(res.priceBtc).toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </ColumnTd>
+                          <ColumnTd>{(parseFloat(res.amount) * parseFloat(res.priceBtc)).toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}</ColumnTd>
+                          <ColumnTd>
+                            <Button onClick={() => {setId(res.id);setModalView(true)}}>
+                              Detalhes
+                            </Button>
+                          </ColumnTd>
+                        </Row>
+                      );
+                    })}
                   </TableBody>
                 </TableResponsive>
               </Content>
@@ -75,7 +136,7 @@ const Deposit_requests: React.FC = () => {
           take={take}
         />
       </Wrapper>
-      <ModalViewRequest modal={modalView} setModal={setModalView} />
+      <ModalViewRequest request={!!id ? data.find(e=> e.id == id): null} modal={modalView} setModal={setModalView} />
     </LayoutFragment>
   );
 };
